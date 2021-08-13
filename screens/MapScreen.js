bbee6aac2d8d35093aa44594983c2c1d0ea9aa94
annin-tofu <yuok5770@gmail.com>
2021-08-13 22:04:50 +0900

    bugfix

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -4,8 +4,11 @@ import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
 import { createStackNavigator } from "@react-navigation/stack";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
+
   return (
     <View>
       {/* Upper Half of MapScreen is for "Map" */}
@@ -18,6 +21,11 @@ const MapScreen = () => {
           <Stack.Screen
             name="NavigateCard"
             component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
