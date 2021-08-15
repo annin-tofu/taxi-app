80835c9a7a6dcb9dcb6acd527e6d0ad492c1ea26
annin-tofu <yuok5770@gmail.com>
2021-08-15 23:11:47 +0900

    RideOptinonsCard created

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
+import { useNavigation } from "@react-navigation/native";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Pick a ride!</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={[tw`absolute top-3 left-5 z-50 p-3 rounded-full`]}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default RideOptionsCard;
-
-const styles = StyleSheet.create({});
